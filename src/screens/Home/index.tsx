83193462a62407f7b10e7d0010fc26717fdca1df
malak <malak.abogdaya@gmail.com>
2023-12-17 15:28:29 +0200

    remove timeout

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { View } from "react-native";
 import Title from "../../components/Title";
 
 function Home(): JSX.Element {
-    const [title, setTitle] = useState('My first component');
-
-    useEffect(() => {
-        setTimeout(() => {
-            setTitle("Title updated");
-        }, 5000);
-    }, []);
-
-    console.log("home component got build");
-
 
     return (
         <View>
